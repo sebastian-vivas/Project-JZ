cd5df476071cbaa20e6d03ca53165ecf0294c011
Sebastian Vivas <seb.vivas.dev@gmail.com>
2025-08-29 23:52:36 -0400

    Updated landing and info pages to account for no bootstrap

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -8,25 +8,26 @@ const Landing = () => {
     <>
       <section className="container">
         <HeaderNav />
-        <section className="landing-welcome-section">
+        <section className="welcome-section">
           <h1 className="landing-heading">Hi, my name is Julissa.</h1>
           <h2 className="landing-secondary-heading">
-            Currently working as a Product Designer II at New Visions for Public
-            Schools, designing a school management application for educators
-            across NYC.
-            <span className="landing-secondary-heading-line-break">
+            Currently working as a Product Designer II at{" "}
+            <span className="company-name">New Visions for Public Schools</span>
+            , designing a school management application for educators across
+            NYC.
+            <span className="secondary-heading-line-break">
               Also an avid plant grower, amateur ceramicist, and Peruvian food
               enthusiast
             </span>
           </h2>
-          <section className="landing-selected-work">
+          <section className="selected-work">
             <section>
               <img
                 src={downArrowIcon}
                 alt="Down arrow"
                 className="down-arrow"
               />
-              <h3 className="landing-selected-work-text">SELECTED WORK</h3>
+              <h3 className="selected-work-text">SELECTED WORK</h3>
               <img
                 src={downArrowIcon}
                 alt="Down arrow"
@@ -35,7 +36,7 @@ const Landing = () => {
             </section>
           </section>
         </section>
-        <section className="landing-case-studies-container">
+        <section className="case-studies-container">
           <CaseStudy route="studentProfile" />
           <CaseStudy />
           <CaseStudy />
